4b5304d0c14d201dc22792f256a73d933e90e0d9
Cesar Zavaleta Estrada <zavaletap2210@gmail.com>
2025-05-07 13:31:09 -0600

    Singleton con ASP. Tools -> Biblioteca de clases

---
+++ Tools/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tools
+{
+    public sealed class Log //sealed -> No permite heredar
+    {
+        private static Log _instance = null;
+        private string _path;
+
+        public static Log GetInstance(string path)
+        {
+            if (_instance == null)
+            {
+                _instance = new Log(path);
+            }
+            return _instance;
+        }
+        private Log(string path) 
+        {
+            _path = path;
+        }
+        public void Save(string message)
+        {
+            File.AppendAllText(_path, message + Environment.NewLine);
+        }
+    }
+}
