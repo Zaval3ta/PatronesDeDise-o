1613beb0fe8288fc59bd6d3858483eebbe469270
Cesar Zavaleta Estrada <zavaletap2210@gmail.com>
2025-05-06 22:24:53 -0600

    Singleton

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using DesignPattern.Singleton;
+
+//var singleton = Singleton.Instance;
+var log = Log.Instance;
+log.Save("Singleton");
+log.Save("Singleton 2");
+//Para comprobar que es el mismo objeto
+var a = Singleton.Instance;
+var z = Singleton.Instance;
+
+Console.WriteLine(a == z);// -> True
---
+++ DesignPattern/Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Singleton
+{
+    public class Singleton
+    {
+        //readonly -> Solo se puede leer(get). No se puede modificar(set).
+        //static -> Pertenece a la clase NO al objeto.
+        // = new Singleton() -> Se crea el objeto al momento que se compila el proyecto.
+        private readonly static Singleton _instance = new Singleton();
+        //Propiedad para acceder al unico objeto de la clase
+        public static Singleton Instance
+        {
+            get { return _instance; }
+        }
+
+        //Constructor privado para que no se puedan crear objetos.
+        private Singleton() { } 
+        
+    }
+}
