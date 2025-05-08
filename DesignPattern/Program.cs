37ccbc8fe7d6b676fefe367103f932731a78ff53
Cesar Zavaleta Estrada <zavaletap2210@gmail.com>
2025-05-07 18:28:34 -0600

    Factory

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,4 +1,5 @@
-﻿using DesignPattern.Singleton;
+﻿using DesignPattern.FactoryPattern;
+using DesignPattern.Singleton;
 
 //var singleton = Singleton.Instance;
 var log = Log.Instance;
@@ -8,4 +9,16 @@ log.Save("Singleton 2");
 var a = Singleton.Instance;
 var z = Singleton.Instance;
 
-Console.WriteLine(a == z);// -> True
+Console.WriteLine(a == z);// -> True 
+//SaleFactory -> Clase abstracta (Creador)
+//new StoreSaleFactory(); -> Fabrica de objetos
+SaleFactory storeSaleFactory = new StoreSaleFactory(10);
+//new InternetSaleFactory(); -> Fabrica de objetos
+SaleFactory internetSaleFactory = new InternetSaleFactory(5);
+
+//Creacion de objetos con el uso de las fabricas
+ISale sale1 = storeSaleFactory.GetSale();
+sale1.Sell(15);
+
+ISale sale2 = internetSaleFactory.GetSale();
+sale2.Sell(15);
