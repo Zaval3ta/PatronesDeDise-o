8fe6c4ddd9bdd26f3720089525e72a863566daaf
Cesar Zavaleta Estrada <zavaletap2210@gmail.com>
2025-05-07 20:28:43 -0600

    Patron de diseño Factory en ASP

---
+++ DesignPatternsAsp/Controllers/ProductDetailController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tools.Earn;
+
+namespace DesignPatternsAsp.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        public IActionResult Index(decimal total)
+        {
+            //Factory
+            LocalEarnFactory localEarnFactory = new LocalEarnFactory(0.20m);
+            ForeignEarnFactory foreignEarnFactory = new ForeignEarnFactory(0.30m, 20);
+            //Producto
+            var localEarn = localEarnFactory.GetEarn();
+            var foreignEarn = foreignEarnFactory.GetEarn();
+            //total
+            ViewBag.totalLocal = total + localEarn.Earn(total);
+            ViewBag.totalForeign = total + foreignEarn.Earn(total);
+
+            return View();
+        }
+    }
+}
---
+++ Tools/Earn/IEarn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Earn
+{
+    //Producto(factorizar nuestro factory)
+    public interface IEarn
+    {
+        public decimal Earn(decimal amount);
+
+    }
+}
